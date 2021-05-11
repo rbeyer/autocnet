11c33923838a05555fa41a6cfab4ff6d0dcb94e1
Kelvin Rodriguez <kr788@nau.edu>
2021-05-11 10:21:14 -0700

    ticked version (#555)
    * ticked version
    
    * ticked recipe version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def setup_package():
 
     setup(
         name = "autocnet",
-        version = '0.2.7',
+        version = '0.4.0',
         author = "Jay Laura",
         author_email = "jlaura@usgs.gov",
         description = ("I/O API to support planetary data formats."),
